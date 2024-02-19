f10b91d0f36af373d33ee30438ed5a1e1e45ae06
Valentyn <valikmarusak264@gmail.com>
2024-02-19 15:48:35 +0000

    modul 2. Autocheck-15

--- main.py
+++ main.py
@@ -1,5 +1,78 @@
-print('hello world')
+result = None
+operand = None
+operator = None
+wait_for_number = True
 
-print('Hello World again')
+while wait_for_number:
+    try:
+        operand = float(input("Enter your number: "))
+        result = operand
+        print(operand)
+        break
+    except ValueError:
+        print("Enter a number! ")
 
-print('Hello Git')
+
+while True:
+    operator = input("Enter an operator (/ * + -) or (=): ")
+    
+    if operator == "*":
+
+        while wait_for_number:
+            try:
+                operand = float(input("Enter your number: "))
+                result = result * operand
+                print(operand)
+                break
+            except ValueError:
+                print("Enter a number! ")
+
+    elif operator == "+":
+
+        while wait_for_number:
+            try:
+                operand = float(input("Enter your number: "))
+                result = result + operand
+                print(operand)
+                break
+            except ValueError:
+                print("Enter a number! ")
+
+    elif operator == "-":
+
+        while wait_for_number:
+            try:
+                operand = float(input("Enter your number: "))
+                result = result - operand
+                print(operand)
+                break
+            except ValueError:
+                print("Enter a number! ")
+
+    elif operator == "/":
+
+        while wait_for_number:
+            try:
+                operand = float(input("Enter your number: "))
+                if operand == 0:
+                    print("You cannot devide by zero!")
+                    continue
+                else:
+                    result = result / operand
+                    print(operand)
+                    break
+            except ValueError:
+                print("Enter a number! ")
+
+    elif operator == "=":
+        print(result)
+        break
+
+    else:
+        print(f"{operator} is not (/ * + - =)")
+        
+
+
+    
+    
+    
